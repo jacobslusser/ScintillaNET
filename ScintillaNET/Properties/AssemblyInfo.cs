e1256bf737c7dca011a3667710b4b05dcdb7517a
Jacob Slusser <jslusser.mobile@gmail.com>
2015-06-05 18:44:51 -0700

    3.4.0 Release.

--- ScintillaNET/Properties/AssemblyInfo.cs
+++ ScintillaNET/Properties/AssemblyInfo.cs
@@ -35,9 +35,11 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.0.0")]
-[assembly: AssemblyFileVersion("3.5.0.0")]
-[assembly: AssemblyInformationalVersion("3.5.0-rc")]
+[assembly: AssemblyVersion("3.4.0.0")]
+[assembly: AssemblyFileVersion("3.4.0.0")]
+[assembly: AssemblyInformationalVersion("3.4.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
+#if (DEBUG)
 [assembly: InternalsVisibleToAttribute("ScintillaNET.Test")]
+#endif
