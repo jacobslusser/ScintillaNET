c5186f785a7b5f1ac94b2ad1b5bfeb976d9af911
Ryan O'Day <romanguy722@gmail.com>
2015-07-01 22:42:01 -0400

    Converted SelectedText into a property
    Unsure how to deal with parameters of DirectMessage

--- src/ScintillaNET/Scintilla.cs
+++ src/ScintillaNET/Scintilla.cs
@@ -4334,28 +4334,26 @@ namespace ScintillaNET
         }
 
         /// <summary>
-        /// Gets the selected text.
+        /// Gets or sets the selected text.
         /// </summary>
         /// <returns>The selected text if there is any; otherwise, an empty string.</returns>
         [Browsable(false)]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public unsafe string SelectedText
-        {
-            get
-            {
-                // NOTE: For some reason the length returned by this API includes the terminating NULL
-                var length = DirectMessage(NativeMethods.SCI_GETSELTEXT).ToInt32() - 1;
-                if (length <= 0)
-                    return string.Empty;
+        public unsafe string SelectedText {
+	get {
+		// NOTE: For some reason the length returned by this API includes the terminating NULL
+		dynamic length = DirectMessage(NativeMethods.SCI_GETSELTEXT).ToInt32() - 1;
+		if (length <= 0) {
+			return string.Empty;
+		}
 
-                var bytes = new byte[length + 1];
-                fixed (byte* bp = bytes)
-                {
-                    DirectMessage(NativeMethods.SCI_GETSELTEXT, IntPtr.Zero, new IntPtr(bp));
-                    return Helpers.GetString(new IntPtr(bp), length, Encoding);
-                }
-            }
-        }
+		dynamic bytes = new byte[length + 1];
+		DirectMessage(NativeMethods.SCI_GETSELTEXT, IntPtr.Zero, new IntPtr(bp));
+		return Helpers.GetString(new IntPtr(bp), length, Encoding);
+
+	}
+	set { DirectMessage(Constants.SCI_REPLACESEL, IntPtr.Zero, Text); }
+}
 
         /// <summary>
         /// Gets or sets whether to fill past the end of a line with the selection background color.
