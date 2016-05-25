d396f7e50ec669722ae98d77898aef0ad0357c74
Jacob Slusser <jslusser.mobile@gmail.com>
2016-05-24 17:51:45 -0700

    Updated SciLexer.dll to v3.6.6.
    No functional changes.

--- src/ScintillaNET/Properties/AssemblyInfo.cs
+++ src/ScintillaNET/Properties/AssemblyInfo.cs
@@ -35,9 +35,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.9.0")]
-[assembly: AssemblyFileVersion("3.5.9.0")]
-[assembly: AssemblyInformationalVersion("3.5.9")]
+[assembly: AssemblyVersion("3.5.10.0")]
+[assembly: AssemblyFileVersion("3.5.10.0")]
+[assembly: AssemblyInformationalVersion("3.5.10")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 #if (DEBUG)
