2e5b28e0f5dff8c7c9de0eecd769aaf2e0d737bd
jacobslusser <jslusser.mobile@gmail.com>
2017-02-18 17:03:25 -0800

    3.6.3 Release

--- src/ScintillaNET/Properties/AssemblyInfo.cs
+++ src/ScintillaNET/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jacob Slusser")]
 [assembly: AssemblyProduct("ScintillaNET")]
-[assembly: AssemblyCopyright("Copyright (c) 2016, Jacob Slusser. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2017, Jacob Slusser. All rights reserved.")]
 [assembly: AssemblyTrademark("The MIT License (MIT)")]
 [assembly: AssemblyCulture("")]
 
@@ -35,9 +35,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.6.2.0")]
-[assembly: AssemblyFileVersion("3.6.2.0")]
-[assembly: AssemblyInformationalVersion("3.6.2")]
+[assembly: AssemblyVersion("3.6.3.0")]
+[assembly: AssemblyFileVersion("3.6.3.0")]
+[assembly: AssemblyInformationalVersion("3.6.3")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 #if (DEBUG)
