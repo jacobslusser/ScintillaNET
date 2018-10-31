84f0a55e13f58e7d5360af3abc826c41730f1954
jacobslusser <jslusser.mobile@gmail.com>
2018-10-31 10:48:15 -0700

    Preparing for new 4.x development. Updated SciLexer.dll and version numbers.

--- src/ScintillaNET/Properties/AssemblyInfo.cs
+++ src/ScintillaNET/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jacob Slusser")]
 [assembly: AssemblyProduct("ScintillaNET")]
-[assembly: AssemblyCopyright("Copyright (c) 2017, Jacob Slusser. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2018, Jacob Slusser. All rights reserved.")]
 [assembly: AssemblyTrademark("The MIT License (MIT)")]
 [assembly: AssemblyCulture("")]
 
@@ -35,9 +35,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.6.3.0")]
-[assembly: AssemblyFileVersion("3.6.3.0")]
-[assembly: AssemblyInformationalVersion("3.6.3")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyInformationalVersion("4.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 #if (DEBUG)
